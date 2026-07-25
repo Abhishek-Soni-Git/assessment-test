aadf503a88ab57d89eed4964651421da0593827d
Abhishek-Soni-Git <soniabhishek4567@gmail.com>
2026-07-25 13:50:25 +0530

    Fix main.tf - correct resource group references

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -31,7 +31,7 @@ variable "node_count" {
 variable "vm_size" {
   description = "VM size for AKS nodes"
   type        = string
-  default     = "Standard_B2s"
+  default     = "Standard_B2s_v2"
 }
 
 variable "acr_sku" {
